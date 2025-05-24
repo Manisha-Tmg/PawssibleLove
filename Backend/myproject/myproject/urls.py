8b53f69dafebe86ee4cee95715e11bc87bf9de77
manishatamang752@gmail.com <manishatamang752@gmail.com>
2025-05-24 12:00:00 +0545

    Created models PetSerializer

--- Backend/myproject/myproject/urls.py
+++ Backend/myproject/myproject/urls.py
@@ -1,7 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
+from myapp.user_views import PetCreateSet,PetDetailView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path()
 ]
+
+
