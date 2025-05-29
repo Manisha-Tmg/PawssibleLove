e6503aaeb6f3f9e420df3d3ab03dffb94af138a4
manishatamang752@gmail.com <manishatamang752@gmail.com>
2025-05-29 23:56:25 +0545

    Created urls

--- Backend/myproject/myproject/urls.py
+++ Backend/myproject/myproject/urls.py
@@ -6,7 +6,8 @@ from myapp.user_views import PetCreateSet,PetDetailView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/pets/',PetCreateSet.as_view(),name='pet-list-create')
+    path('api/pets/',PetCreateSet.as_view(),name='pet-list-create'),
+    path('api/pets/<int:pet_id',PetDetailView.as_view(),name='pet-details')
 ]
 
 
